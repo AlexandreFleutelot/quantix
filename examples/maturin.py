8706deb2f99a160ae2dfd068cd7cb58c521fe4c9
AlexandreFleutelot <fleutelot.alexandre@gmail.com>
2026-01-18 00:31:33 +0100

    refactor maturin example to use a main function and add __main__ guard

--- examples/maturin.py
+++ examples/maturin.py
@@ -2,6 +2,9 @@ import quantix
 
 # Uses the lib.rs
 # lib must be build and installed first with "maturin develop"
+def main():
+    out = quantix.check_gpu()
+    print(out)
 
-out = quantix.check_gpu()
-print(out)
+if __name__ == "__main__":
+    main()
